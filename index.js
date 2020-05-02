04c4591ee39ada97d9105a511c17b61a8e007057
belal <belal20795@gmail.com>
2020-05-02 20:58:43 +0530

    added second challege and change the coment

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let name = prompt("what is ur name");
-let lengthName = name.lengthName
-let nameadd = name.slice(1, lengthName);
-let nameSlice = name.slice(0,1);
-let nameToUpper = nameSlice.toUpperCase();
-alert("hello" + " " + nameToUpper + nameadd)
+// this was the first challenge of bootcamp 
+// to make the first letter of the name capital
+
+function promt() {
+    let name = prompt("what is ur name");
+    let lengthName = name.lengthName
+    let nameadd = name.slice(1, lengthName);
+    let nameSlice = name.slice(0,1);
+    let nameToUpper = nameSlice.toUpperCase();
+    alert("hello" + " " + nameToUpper + nameadd)
+}
+
+// this was the second challenge of bootcamp
+// To calculate the BMI by using the return and the property space
+
+
+function bmiCaluculator(weight, height) {
+    let bmi = weight/(height*height);
+    return Math.round(bmi) ;
+}
+
+let bmi5 = bmiCaluculator(65, 1.8);
+console.log(bmi5);
+
+// chalenge third waiting.
+
