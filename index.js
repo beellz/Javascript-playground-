1604d39497b640b43a377f20f05738f989f41bf9
belal <belal20795@gmail.com>
2020-05-02 15:57:45 +0530

    added a code bootcamp challenge of first letter uppercase

---
+++ index.js
@@ -0,0 +1,6 @@
+let name = prompt("what is ur name");
+let lengthName = name.lengthName
+let nameadd = name.slice(1, lengthName);
+let nameSlice = name.slice(0,1);
+let nameToUpper = nameSlice.toUpperCase();
+alert("hello" + " " + nameToUpper + nameadd)
